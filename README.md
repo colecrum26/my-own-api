dd21ff3b7a16f835d4a45ace5de534b2c0766381
colecrum26 <cwcrumrine@gmail.com>
2023-04-24 10:14:47 -0500

    brought in seeds and updated README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # my-own-api
-API that queries a database and returns information depending on the data requested
+
+## Description
+This project is a back-end API that queries a database of existing Clients and returns pertinent information depending on the data requested. 
+
+## Installation
+In order to install this project you will need to install all node packages with `npm i`, seed the data base by running `node seeds.js` within the db folder, and then `node server.js` in the root folder to run the server. You could also seed the database by creating JSON files in a post method if you want all custom data.
+
+## Usage
+This program allows data to be written in MySQL in regards to Clients.
---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('6483482', 'Bins-Abbott', 'Industrial Machinery/Components', 'West End', 'Canada');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('4643676', 'Rolfson-Heller', 'Electric Utilities: Central', 'Villeurbanne', 'France');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('8458074', 'Murray, Hoeger and Wisozk', 'Marine Transportation', 'München', 'Germany');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('9766276', 'Dicki-Kuhic', 'Marine Transportation', 'Washington', 'United States');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('5198528', 'Fay-Lakin', 'n/a', 'Hénin-Beaumont', 'France');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('3269174', 'Schaefer-Jerde', 'n/a', 'Orléans', 'France');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('3101333', 'Robel, Gerhold and Weber', 'EDP Services', 'Rayside-Balfour', 'Canada');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('9670500', 'Rolfson Group', 'Specialty Insurers', 'Chattanooga', 'United States');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('0231629', 'Hyatt, Hyatt and Conroy', 'n/a', 'Birmingham', 'United States');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('8012217', 'Wilderman Inc', 'Food Distributors', 'Kugluktuk', 'Canada');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('8549372', 'Reynolds, Marquardt and Carter', 'Coal Mining', 'Mesa', 'United States');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('6383355', 'Brown and Sons', 'Real Estate Investment Trusts', 'Tours', 'France');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('0923146', 'Bogan-Hyatt', 'Telecommunications Equipment', 'Laurel', 'United States');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('3330110', 'Padberg Inc', 'Finance: Consumer Services', 'Arcueil', 'France');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('4647393', 'Pagac and Sons', 'Restaurants', 'Roubaix', 'France');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('8382907', 'Ebert-Crist', 'Oil & Gas Production', 'Royan', 'France');
+insert into Clients (CompanyID, CompanyName, Industry, City, Country) values ('9812165', 'Rice-Witting', 'Business Services', 'Phoenix', 'United States');
