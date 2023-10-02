ee1a37735ebc2866128c7cc034d9aca5de87c318
colecrum26 <cwcrumrine@gmail.com>
2023-10-02 10:54:48 -0500

    formatted

--- controllers/clients.controller.js
+++ controllers/clients.controller.js
@@ -1,34 +1,34 @@
 import query from "../db/utils.js";
 
 const findAll = async () => {
-    return query("SELECT ClientID, ClientName, Industry, City, Country FROM clients");
-  };
-  
-  const findOne = async (id) => {
-    return query("SELECT ClientID, ClientName, Industry, City, Country FROM clients WHERE ClientID = ?", [
-      id,
-    ]);
-  };
-  
-  const addOne = async (client) => {
-      return await query("INSERT INTO clients SET ?", [client]);
-    };
-    
-    const updateOne = async (id, client) => {
-      return await query("UPDATE clients SET ? WHERE ClientID = ?", [
-        client,
-        id,
-      ]);
-    };
-    
-    const removeOne = async (id) => {
-      return await query("DELETE FROM clients WHERE ClientID = ?", [id]);
-    };
-  
-    export default {
-      findAll,
-      findOne,
-      addOne,
-      updateOne,
-      removeOne,
-    };
+  return query(
+    "SELECT ClientID, ClientName, Industry, City, Country FROM clients"
+  );
+};
+
+const findOne = async (id) => {
+  return query(
+    "SELECT ClientID, ClientName, Industry, City, Country FROM clients WHERE ClientID = ?",
+    [id]
+  );
+};
+
+const addOne = async (client) => {
+  return await query("INSERT INTO clients SET ?", [client]);
+};
+
+const updateOne = async (id, client) => {
+  return await query("UPDATE clients SET ? WHERE ClientID = ?", [client, id]);
+};
+
+const removeOne = async (id) => {
+  return await query("DELETE FROM clients WHERE ClientID = ?", [id]);
+};
+
+export default {
+  findAll,
+  findOne,
+  addOne,
+  updateOne,
+  removeOne,
+};
